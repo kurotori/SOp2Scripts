8fa62901af5381c62407e2209f42d7e360280f70
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2023-10-23 10:37:34 -0300

    Actuaización del código

--- ProyectoJuego/administrarJuego.sh
+++ ProyectoJuego/administrarJuego.sh
@@ -18,11 +18,41 @@ ejecutarConsulta()
 #ejecutarConsulta "$usuarioBD" "$contraseniaBD" "SET sql_notes=0;"
 
 info=( $(ejecutarConsulta "$usuarioBD" "$contraseniaBD" "$consulta") )
+opcion=""
+inicio=1
+tope=10
+while [ "$opcion" != "s" ]; do
+    clear
+    for indice in "${!info[@]}"
+    do
+        if [ "$indice" -eq 0 ]; then
+            echo "${info[$indice]}"|sed "s/:/\t/g"
+            echo "----------------------------"
+        else
+            if [ "$indice" -ge "$inicio" ] && [ "$indice" -le "$tope" ] ; then
+                echo "$indice - ${info[$indice]}"|sed "s/:/\t/g"
+            fi
+        fi
+    done
+    echo ""
+    echo "Presione S para salir, D para avanzar, A para retroceder"
+    read opcion
 
-for indice in "${!info[@]}"
-do
-    echo "${info[$indice]}"|sed "s/:/\t/g"
-    if [ "$indice" -eq 0 ]; then
-        echo "----------------------------"
-    fi
+    case "$opcion" in
+        [Ss])
+            opcion=s
+        ;;
+        [dD])
+            tope=$(( $tope+10 ))
+            inicio=$(( $inicio+10 ))
+        ;;
+        [aA])
+            tope=$(( $tope-10 ))
+            inicio=$(( $inicio-10 ))
+            #AGREGAR CHEQUEO DE TOPE
+        ;;
+    esac
+    
+
 done
+
--- ProyectoJuego/administrarJuego_avanzado.sh
+++ ProyectoJuego/administrarJuego_avanzado.sh
@@ -2,52 +2,182 @@
 clear
 
 
-consultaBD()
+
+consulta="select concat_ws(':',nombreU,nombreP,apellidoP)
+ as 'Usuario:Nombre:Apellido' from juego.usuario;"
+
+datosUsuario="select concat_ws(':',ID,nombreU,nombreP,apellidoP)
+as 'ID:Usuario:Nombre:Apellido' from juego.usuario where nombreU="
+
+linea(){
+    long=$(tput cols)
+    linea=""
+    for((i=0;i<$long;i++)); do
+        linea="$linea-"
+    done
+    echo "$linea"
+}
+
+espacio(){
+    for((i=0;i<$1;i++)); do
+        espacios="$espacios "
+    done
+    echo "$espacios"
+}
+
+mostrarTitulo()
 {
+    titulo="$1" 
+    long=$(tput cols)
+    chars=${#titulo}
+    long=$(((long-chars)/2))
+    espacio=$(espacio "$long")
     
+    
+    echo "$espacio$titulo"
+}
+
+encabezado()
+{
+    clear
+    linea
+    mostrarTitulo "Sistema de Administración de Usuarios"
+    linea
+}
+
+ejecutarConsulta()
+{
+    datos=$(mysql -u "$1" -p"$2" -e "$3")
+    echo "$datos"
 }
 
 
+encabezado
+mostrarTitulo "Iniciar Sesión:"
+linea
+echo " Nombre de Usuario:"
+read usuarioBD
+echo " Contraseña:"
+read -s contraseniaBD
 
 
+#ejecutarConsulta "$usuarioBD" "$contraseniaBD" "SET sql_notes=0;"
+opcionI=""
 
-usuarioBD="estudiante"
+while [ "$opcionI" != "s" ]; do
+    encabezado
+    mostrarTitulo "Menú Principal"
+    linea
+    echo "1 - Listado de Usuarios"
+    echo "S - Salir"
+    linea
+    read opcionI
 
+    case "${opcionI}" in
+        1)
+            opcion=""
+            info=( $(ejecutarConsulta "$usuarioBD" "$contraseniaBD" "$consulta") )
 
-consulta="select concat_ws(':',nombreU,ID,nombreP,apellidoP) as 'Usuario:ID:Nombre:Apellido' from juego.usuario;"
-usuarios=$(mysql -u "$usuarioBD" -p -e "$consulta")
+            inicio=1
+            tope=10
+            limite=0
+            
+            while [ "$opcion" != "s" ]; do
+                limite=0
+                encabezado
+                mostrarTitulo "Listado de Usuarios del Sistema:"
+                linea
+                for indice in "${!info[@]}"
+                do
+                    if [ "$indice" -eq 0 ]; then
+                        registro=$(echo "${info[$indice]}"|sed "s/:/\t/g")
+                        printf '%-4s %-10s %-10s %-10s\n' '#' $registro
+                        linea
+                    else
+                        if [ "$indice" -ge "$inicio" ] && [ "$indice" -le "$tope" ] ; then
+                            registro=$(echo "$indice:${info[$indice]}"|sed "s/:/\t/g")
+                            printf '%-4s %-10s %-10s %-10s\n' $registro
+                        fi
+                    fi
+                    limite=$(( $limite+1 ))
+                done
+                linea
+                printf 'Registros %4s hasta %4s, de %4s\n' "$inicio" "$tope" "$limite" 
+                linea
+                echo "Presione S para salir, D para avanzar, A para retroceder"
+                echo "Ingrese un número de registro para ver detalles"
+                read opcion
 
-clear
-#echo "$usuarios"|cut -f 1
+                case "$opcion" in
+                    [Ss])
+                        opcion=s
+                        clear
+                    ;;
+                    [dD])
+                        if [ "$tope" -le "$limite" ]; then  
+                            tope=$(( $tope+10 ))
+                            inicio=$(( $inicio+10 ))  
+                        fi
+                    ;;
+                    [aA])
+                        if [ "$inicio" -gt "1" ]; then
+                            tope=$(( $tope-10 ))
+                            inicio=$(( $inicio-10 ))
+                        fi
+                        #AGREGAR CHEQUEO DE TOPE
+                    ;;
+                    *)
+                        if [[ $opcion =~ ^[0-9]+$ ]]; then
+                            encabezado
+                            echo "Datos del Usuario"
+                            linea
+                            nombreU=$(echo "${info[$opcion]}"|cut -d":" -f1)
+                            infoUsuario=( $(ejecutarConsulta "$usuarioBD" "$contraseniaBD" "${datosUsuario}'${nombreU}'") )
+                            printf '%-10s %-10s\n' "Usuario:" $(echo "${infoUsuario[1]}"|cut -d":" -f2 )
+                            linea
+                            printf '%-10s %-10s\n' "ID:" $(echo "${infoUsuario[1]}"|cut -d":" -f1 )
+                            printf '%-10s %-10s\n' "Nombre:" $(echo "${infoUsuario[1]}"|cut -d":" -f3 )
+                            printf '%-10s %-10s\n' "Apellido:" $(echo "${infoUsuario[1]}"|cut -d":" -f4 )
+                            linea
+                            echo "Presione Enter para continuar"
+                            linea
+                        else
+                            echo "no valido"
+                        fi
+                        
+                        read ok
+                    ;;
+                esac
+                
 
-arrUsuarios=( $(echo "$usuarios") )
-echo "$arrUsuarios"
+            done
+        ;;
+        [Ss])
+            encabezado
+            mostrarTitulo "Salir del Sistema:"
+            linea
+            echo "Va a salir del sistema"
+            echo "¿Desea Continuar?"
+            linea
+            echo "S - SI"
+            echo "N - NO"
+            read opcionI
+            case "${opcionI}" in
+                [sS])
+                    clear
+                    opcionI="s"
+            esac
+            
+            
+        ;;
+        *)
+            echo "default (none of above)"
+        ;;
+    esac
+    
 
-read ok
-
-posI=0
-posF=0
-tope=${#arrUsuarios[@]}
-opcion=""
-
-while [ "$opcion" != "s" ] && [ "$opcion" != "S" ]; do
-    clear
-    echo "Usuarios"
-    echo "--------"
-
-    for((i=0;i<15;i++)); do
-        dato="${arrUsuarios[$i]}"
-        datos=$(echo "$dato"| sed "s/:/\t/g")
-        echo -e "$i\t$datos"
-    done
-
-    read opcion
+    
 done
-
-
-
-
-
 
 
 
